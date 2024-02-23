cac63c3c5b7352c08d507b4729f2c4f382d008ed
Erik H ejh2958 <erik.halldorson@utexas.edu>
2024-02-22 18:40:05 -0600

    all my changes thus far

--- hardwareset/src/App.js
+++ hardwareset/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/home'
+import AboutPage from './pages/about'
+import ErrorPage from './pages/error'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+ <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="./pages/home" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
