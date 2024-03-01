924ee71d8c1ab2e53cc169908541f3def9c0ff9c
Rahman Khandakar <rahmankhandakar22@utexas.edu>
2024-03-01 03:04:37 -0600

    added redirection to projects page  with hardcoded username and password

--- hardwareset/src/App.js
+++ hardwareset/src/App.js
@@ -5,6 +5,7 @@ import HomePage from './pages/home'
 import AboutPage from './pages/about'
 import ErrorPage from './pages/error'
 import RegisterPage from './pages/register'
+import ProjectsPage from './pages/projects'
 function App() {
   return(
  <Router>
@@ -13,6 +14,7 @@ function App() {
         <Route path="./pages/home" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
--- hardwareset/src/components/register.jsx
+++ hardwareset/src/components/register.jsx
@@ -19,9 +19,7 @@ function Register() {
         try {
             const response = await fetch('/register', {
                 method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
+                headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({name, username, password })
             });
             const responseData = await response.json();
