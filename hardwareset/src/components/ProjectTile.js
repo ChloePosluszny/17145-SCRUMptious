f3e14792f17e2ca848dc9480d31bfa7921782ac5
Spencer Keen <spencerkeen27@gmail.com>
2024-03-08 01:59:41 -0600

    Plugged in the Projects Page

---
+++ hardwareset/src/components/ProjectTile.js
@@ -0,0 +1,40 @@
+export default function ProjectTile ({children, project}) {
+    return (
+        <>
+            <div style={styles.projectTile}>
+                <p style={styles.projectName}>{project.name}</p>
+                <p style={styles.projectUsers}>{project.users.toString()}</p>
+                <div style={styles.hardwareSetTilesContainer}>
+                    {children}
+                </div>
+            </div>
+        </>
+    )
+}
+
+const styles = {
+    projectTile: {
+        border: '1px',
+        borderStyle: 'solid',
+        borderColor: 'black',
+        backgroundColor: 'lightgray',
+        padding: '10px',
+        margin: '10px',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+
+    hardwareSetTilesContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+
+    projectName: {
+        margin: '5px'
+    },
+
+    projectUsers: {
+        margin: '5px'
+    }
+};
