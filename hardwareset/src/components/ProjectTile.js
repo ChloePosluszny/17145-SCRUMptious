c6481aad23f818a4f5fce93aa62f176e2860d290
Rahman Khandakar <rahmankhandakar22@utexas.edu>
2024-04-05 12:36:30 -0500

    css to project tile

--- hardwareset/src/components/ProjectTile.js
+++ hardwareset/src/components/ProjectTile.js
@@ -18,11 +18,14 @@ const styles = {
         borderStyle: 'solid',
         borderColor: 'black',
         backgroundColor: 'lightgray',
+        
         padding: '10px',
         margin: '10px',
         display: 'flex',
         flexDirection: 'row',
         alignItems: 'center',
+        boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)', 
+        borderRadius: '8px', 
     },
 
     hardwareSetTilesContainer: {
